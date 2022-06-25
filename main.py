d7c7f2f687a3a3502cd36db65a4d627e9be4029f
Ascetic <3038037456@qq.com>
2022-06-25 18:53:19 +0800

    202206251852

--- main.py
+++ main.py
@@ -2,17 +2,24 @@ import os
 import shutil
 
 # 打开相关文件
-f_in = open("task1.trainSentence.new", encoding='utf_8')
-f_out = open("exp1_1.txt", "w", encoding='utf_8')
-# 按行读取文件内容
-for line in f_in:
-    # 用制表符分割每行内容并赋值给列表
-    lineData = line.split('\t')
-    # 只要前五个元素
-    f_out.write(lineData[0] + "\t" + lineData[1] + "\t" + lineData[2] + "\t" + lineData[3] + "\t" + lineData[4] + "\n")
-# 关闭相关文件
-f_in.close()
-f_out.close()
+try:
+    f_in = open("task1.trainSentence.new", encoding='utf_8')
+    f_out = open("exp1_1.txt", "w", encoding='utf_8')
+except FileNotFoundError:
+    print("ERROR!TestFileNotFound!")
+    exit(-1)  # 出现异常终止程序
+else:
+    print("FileOpenSuccess!")
+    # 按行读取文件内容
+    for line in f_in:
+        # 用制表符分割每行内容并赋值给列表
+        lineData = line.split('\t')
+        # 只要前五个元素
+        f_out.write(
+            lineData[0] + "\t" + lineData[1] + "\t" + lineData[2] + "\t" + lineData[3] + "\t" + lineData[4] + "\n")
+    # 关闭相关文件
+    f_in.close()
+    f_out.close()
 
 # 判断文件是否存在
 if os.path.exists("exp1_train"):
@@ -26,28 +33,40 @@ if os.path.exists("exp1_train"):
 else:
     # 如果不存在则直接创建
     os.mkdir("exp1_train")
-# 将文件夹中的文件拷贝到新文件夹中
-shutil.copy("task1.trainSentence.new", "exp1_train")
-shutil.copy("task1.test.new", "exp1_train")
+
+try:
+    # 将文件夹中的文件拷贝到新文件夹中
+    shutil.copy("task1.trainSentence.new", "exp1_train")
+    shutil.copy("task1.test.new", "exp1_train")
+except FileNotFoundError:
+    print("ERROR!TestFileNotFound!")
+    exit(-1)  # 出现异常终止程序
+else:
+    print("FileCopySuccess!")
 # 更换新的工作目录并打开相关文件
 os.chdir("exp1_train")
-f_in = open("task1.trainSentence.new", encoding='utf_8')
-
 # 用于存储关系类型
 relationshipType = []
 
-# 按行读取文件内容
-for line in f_in:
-    # 用制表符分割每行内容并赋值给列表
-    lineData = line.split('\t')
-    # 判断当前关系类型是否已经存在于存储关系的列表中，若存在就跳过
-    if lineData[0] in relationshipType:
-        continue
-    else:
-        # 若不存在那么将当前关系类型添加到存储关系的列表中
-        relationshipType.append(lineData[0])
-# 关闭相关文件
-f_in.close()
+try:
+    f_in = open("task1.trainSentence.new", encoding='utf_8')
+except FileNotFoundError:
+    print("ERROR!TestFileNotFound!")
+    exit(-1)  # 出现异常终止程序
+else:
+    print("FileOpenSuccess!")
+    # 按行读取文件内容
+    for line in f_in:
+        # 用制表符分割每行内容并赋值给列表
+        lineData = line.split('\t')
+        # 判断当前关系类型是否已经存在于存储关系的列表中，若存在就跳过
+        if lineData[0] in relationshipType:
+            continue
+        else:
+            # 若不存在那么将当前关系类型添加到存储关系的列表中
+            relationshipType.append(lineData[0])
+    # 关闭相关文件
+    f_in.close()
 # 获取关系列表的长度也就是关系类型的个数
 relationshipTypeNum = len(relationshipType)
 # 初始化一个变量用于记录行数和循环运行次数
@@ -59,20 +78,24 @@ while i < relationshipTypeNum:
     # 用循环次数的变量名作为文件名
     fileName = str(i) + '.txt'
     # 打开相关文件
-    f_out = open(fileName, "w", encoding='utf_8')
-    f_in = open("task1.trainSentence.new", encoding='utf_8')
-    # 按行读取文件内容
-    for line in f_in:
-        # 用制表符分割每行内容并赋值给列表
-        lineData = line.split('\t')
-        # 判断当前关系类型是否等于当前循环查找的关系类型
-        if lineData[0] == relationshipType[i - 1]:
-            # 如果是则将当前行写入文件
-            f_out.write(lineData[0] + "\t" + lineData[1] + "\t" + lineData[2] + "\t" + lineData[3] + "\t" + lineData[
-                4] + "\t""\n")
-    # 关闭相关文件
-    f_in.close()
-    f_out.close()
+    try:
+        f_out = open(fileName, "w", encoding='utf_8')
+        f_in = open("task1.trainSentence.new", encoding='utf_8')
+    except FileNotFoundError:
+        print("ERROR!TestFileNotFound!")
+        exit(-1)  # 出现异常终止程序
+    else:
+        # 按行读取文件内容
+        for line in f_in:
+            # 用制表符分割每行内容并赋值给列表
+            lineData = line.split('\t')
+            # 判断当前关系类型是否等于当前循环查找的关系类型
+            if lineData[0] == relationshipType[i - 1]:
+                # 如果是则将当前行写入文件
+                f_out.write(lineData[0] + "\t" + lineData[1] + "\t" + lineData[2] + "\t" + lineData[3] + "\t" + lineData[4] + "\t""\n")
+        # 关闭相关文件
+        f_in.close()
+        f_out.close()
 
 # 把工作路径转移到上一级目录
 os.chdir("../")
@@ -89,7 +112,14 @@ else:
     # 如果不存在则直接创建
     os.mkdir("exp1_test")
 # 将文件夹中的文件拷贝到新文件夹中
-shutil.copy("task1.test.new", "exp1_test")
+try:
+    shutil.copy("task1.test.new", "exp1_test")
+except FileNotFoundError:
+    print("ERROR!TestFileNotFound!")
+    exit(-1)  # 出现异常终止程序
+else:
+    print("FileCopySuccess!")
+
 os.chdir("exp1_test")
 
 # 初始化一个变量用于记录行数和循环运行次数
@@ -103,24 +133,31 @@ while i < relationshipTypeNum:
     # 用于创建索引的名字序号
     index = "index" + str(i) + '.txt'
     # 打开相关文件
-    f_out = open(fileName, "w", encoding='utf_8')
-    f_in = open("task1.test.new")
-    f_index = open(index, "w", encoding='utf_8')
-    # 记录发现关系类型的行数用于建立索引
-    k = 1
-    # 按行读取文件内容
-    for line in f_in:
-        # 用制表符分割每行内容并赋值给列表
-        lineData = line.split('\t')
-        # 判断当前关系类型是否等于当前循环查找的关系类型
-        if lineData[0] == relationshipType[i - 1]:
-            # 如果是则将当前行写入文件
-            f_out.write(lineData[0] + "\t" + lineData[1] + "\t" + lineData[2] + "\t" + lineData[3])
-            # 将当前行的索引写入索引文件
-            f_index.write(str(k) + "\n")
-        # 每次读取一行后记录当前行数
-        k = k + 1
-    # 关闭相关文件
-    f_in.close()
-    f_out.close()
-    f_index.close()
+    try:
+        f_out = open(fileName, "w", encoding='utf_8')
+        f_in = open("task1.test.new")
+        f_index = open(index, "w", encoding='utf_8')
+    except FileNotFoundError:
+        print("ERROR!TestFileNotFound!")
+        exit(-1)  # 出现异常终止程序
+    else:
+        print("FileOpenSuccess!")
+        # 记录发现关系类型的行数用于建立索引
+        k = 1
+        # 按行读取文件内容
+        for line in f_in:
+            # 用制表符分割每行内容并赋值给列表
+            lineData = line.split('\t')
+            # 判断当前关系类型是否等于当前循环查找的关系类型
+            if lineData[0] == relationshipType[i - 1]:
+                # 如果是则将当前行写入文件
+                f_out.write(lineData[0] + "\t" + lineData[1] + "\t" + lineData[2] + "\t" + lineData[3])
+                # 将当前行的索引写入索引文件
+                f_index.write(str(k) + "\n")
+            # 每次读取一行后记录当前行数
+            k = k + 1
+        # 关闭相关文件
+        f_in.close()
+        f_out.close()
+        f_index.close()
+print("SystemRunEnd!")
